3b42d5a8fa6b74f28d84baaceffc7ae7ff95af8f
unknown <muhammadabdullahimtiaz4@gmail.com>
2025-05-02 15:36:42 +0500

    Substraction functionality added

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,8 @@ int a=10 , b=15;
 int sum;
 sum = a + b;
 Console.WriteLine($" The sum of two numbers is : {sum} ");
+
+int a=10 , b=15;
+int sub;
+sum = a - b;
+Console.WriteLine($" The substraction of two numbers is : {sub} ");
